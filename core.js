53caf140727ccf761577a6e3a895329dcc770627
Duy Nguyen <duy.v.nguyen.gt@gmail.com>
2016-04-17 16:52:37 -0700

    show rating

--- core.js
+++ core.js
@@ -2,7 +2,7 @@
 var storage = chrome.storage.local;
 var message = document.querySelector('#message');
 var restMap = {};
-
+var clicked = false;
 // Unique ID for the className.
 var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
 
@@ -26,36 +26,66 @@ document.addEventListener('mousemove', function (e) {
 
 //get list of all restaurant
 document.getElementById('desktop-sidebar').addEventListener("click", function (event) {
-    var temp = document.getElementsByClassName("tablesorter");
-    var table = temp[0];
+    if (!clicked) {
+        clicked = true;
+        var temp = document.getElementsByClassName("tablesorter");
+        var table = temp[0];
 
-   for (var i = 1, row; row = table.rows[i]; i++) {
-       var item = row.cells[0];
-       var restName = item.textContent.trim();
+        for (var i = 1, row; row = table.rows[i]; i++) {
+            var item = row.cells[0];
+            var restName = item.textContent.trim();
 
-       //if (typeof restMap[restName] != 'undefined') {
-       if(!(restName in restMap)){
-           restMap[restName] = {
-               link: item.childNodes[1].href,
-               datalat: row.attributes[6].nodeValue,
-               datalng: row.attributes[7].nodeValue,
-               phone: 0,
-               rating: 0.0,
-               url: ""
-           };
-       }
-   }
+            //if (typeof restMap[restName] != 'undefined') {
+            if (!(restName in restMap)) {
+                restMap[restName] = {
+                    link: item.childNodes[1].href,
+                    datalat: row.attributes[6].nodeValue,
+                    datalng: row.attributes[7].nodeValue,
+                    phone: 0,
+                    rating: 0.0,
+                    url: ""
+                };
+            }
+        }
 
-    for (var rest in restMap) {
-        console.log(rest);
-        console.log("lat: " + restMap[rest].datalat);
-        console.log("lng: " + restMap[rest].datalng);
+        for (var rest in restMap) {
+            console.log(rest);
+            console.log("lat: " + restMap[rest].datalat);
+            console.log("lng: " + restMap[rest].datalng);
+        }
+
+        injectRating(table);
+        changeStyle();
     }
 }, true);
 
 function decodeString(encoded) {
    var decoded = encoded.replace(/&amp;/g, '&');
    return decoded;
+};
+
+function injectRating(table) {
+    var tHead = table.tHead;
+    for (var h = 0; h < tHead.rows.length; h++) {
+        var newTH = document.createElement('th');
+        tHead.rows[h].appendChild(newTH);
+        newTH.innerHTML = 'Rating';
+        newTH.className = 'cuisines';
+    }
+
+    var tBodies = table.tBodies[0];
+    for (var i = 0; i < tBodies.rows.length; i++) {
+        var newCell = tBodies.rows[i].insertCell(-1);
+        var temp = table.rows[i+1].cells[0].textContent.trim();
+        //newCell.innerHTML = restMap[temp].rating;
+        newCell.innerHTML = '<img src="https://s3-media1.fl.yelpcdn.com/assets/2/www/img/5ef3eb3cb162/ico/stars/v1/stars_3_half.png" width=95%>';
+        newCell.className = 'desktop-only';
+    }
+};
+
+function changeStyle() {
+    document.getElementsByClassName('cuisine')[0].style.width = '17%';
+    document.getElementsByClassName('neighborhood')[0].style.width = '24%';
 };
 
 //check to see if the ratings have already been pulled, 
@@ -67,16 +97,39 @@ storage.get('css', function (items) {
    if (items.css) {
        chrome.tabs.insertCSS({ code: items.css }, function () {
            if (chrome.runtime.lastError) {
-               message.innerText = 'Something went wrong with the injection process.';
+               console.log ('Something went wrong with the injection process.');
            } else {
-               message.innerText = 'Inject successfully';
+               console.log ('Inject successfully');
            }
        });
    } else {
        //define how the rating are shown in the web page
-       var optionsUrl = chrome.extension.getURL('options.html');
-       message.innerHTML = 'Set CSS in the <a target="_blank" href="' +
-           optionsUrl + '">options page</a>.';
+       //var optionsUrl = chrome.extension.getURL('options.html');
+       //message.innerHTML = 'Set CSS in the <a target="_blank" href="' +
+       //    optionsUrl + '">options page</a>.';
+       var cssCode = '#application table .cuisine { width: 17%; !important } #application table .neighborhood { width: 23%; !important}';
+
+       storage.set({ 'css': cssCode }, function () {
+           // Notify that we saved.
+           console.log('Settings saved');
+       });
+
+       storage.get('css', function (items) {
+           // To avoid checking items.css we could specify storage.get({css: ''}) to
+           // return a default value of '' if there is no css value yet.
+           if (items.css) {
+               textarea.value = items.css;
+               message('Loaded saved CSS.');
+           }
+       });
+
+       chrome.tabs.insertCSS({ code: items.css }, function () {
+           if (chrome.runtime.lastError) {
+               console.log ('Something went wrong with the injection process.');
+           } else {
+               message.innerText ('Inject successfully');
+           }
+       });
    }
 });
 
