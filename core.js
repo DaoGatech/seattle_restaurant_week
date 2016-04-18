91e5080af594b90ec8278277de2cce4fddd75ce5
Thong Dao <tmizzle2005@Thongs-MBP.attlocal.net>
2016-04-17 21:11:48 -0400

    Ajax

--- core.js
+++ core.js
@@ -47,12 +47,46 @@ document.getElementById('desktop-sidebar').addEventListener("click", function (e
    }
 
    for (var rest in restMap) {
-       console.log(rest);
-       console.log("lat: " + restMap[rest].datalat);
-       console.log("lng: " + restMap[rest].datalng);
+	   var near = 'Seattle';
+	   var accessor = {
+	        consumerSecret : token.consumerSecret,
+		    tokenSecret : token.tokenSecret
+	    };
+		parameters = [];
+		parameters.push(['term', rest]);
+		parameters.push(['location', near]);
+        parameters.push(['limit', 1]);
+		parameters.push(['oauth_consumer_key', token.consumerKey]);
+		parameters.push(['oauth_consumer_secret', token.consumerSecret]);
+		parameters.push(['oauth_token', token.accessToken]);
+        parameters.push(['callback', 'cb']);
+		parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+		var message = {
+		    'action' : 'http://api.yelp.com/v2/search',
+			'method' : 'GET',
+			'parameters' : parameters
+		};
+		OAuth.setTimestampAndNonce(message);
+		OAuth.SignatureMethod.sign(message, accessor);
+		var parameterMap = OAuth.getParameterMap(message.parameters);
+		parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+			$.ajax({
+				'url' : message.action,
+				'data' : parameterMap,
+				'cache' : true,
+				'dataType' : 'jsonp',
+				'jsonpCallback' : 'cb',
+				'success' : function(data, textStats, XMLHttpRequest) {
+					console.log(data.length);
+					var output = prettyPrint(data);
+				}
+			});
    }
 }, true);
 
+function cb(data) {
+    console.log(data);
+}
 function decodeString(encoded) {
    var decoded = encoded.replace(/&amp;/g, '&');
    return decoded;
